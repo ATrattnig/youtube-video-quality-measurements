f4ad39c5c1fd96deb971ba88cee8291e52ae1884
Armin Trattnig <armin.trattnig@gmx.at>
2016-03-02 20:03:10 +0100

    Updated the uploadMeasurementScript

--- scripts/uploadMeasurement.sh
+++ scripts/uploadMeasurement.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 VIDEO=$1
+EXPECTEDREPS=$2
 
-echo 'VIDEO='$VIDEO
+#echo 'VIDEO='$VIDEO
+#echo 'EXPECTEDREPS='$EXPECTEDREPS
 
-echo 'Start Uploading ..'
+TMPVIDEO="/tmp/"$(date +%s.%N)".mp4"
+
+MP4Box -quiet -add $VIDEO $TMPVIDEO > /dev/null 2>&1
 
 START=$(date +%s.%N)
 
-ID="$(python youtubeAPI/upload_video.py --file $VIDEO)"
+ID="$(python youtubeAPI/upload_video.py --file $TMPVIDEO)"
 
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 
 URL='https://www.youtube.com/watch?v='$ID
 
-echo 'Upload finished. It took '$DIFF' Seconds.'
-echo 'URL: '$URL
+echo 'Upload '$DIFF' s'
+#echo 'URL: '$URL
 
 youtube-dl -q -F $URL > /dev/null 2>&1
 
@@ -29,29 +33,29 @@ done
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 
-echo 'Processing finished. It took '$DIFF' Seconds.'
+echo 'Processing '$DIFF' s'
 
 REPS=('')
 numOfReps=0
 
-while [[ "$numOfReps" -lt "2" ]]
+while [[ "$numOfReps" -lt $EXPECTEDREPS ]]
 do
-	DLRESULT="$(youtube-dl -F $URL | grep 'DASH video')"
+	DLRESULT="$(youtube-dl -F $URL | grep 'DASH')"
 	for rep in "${REPS[@]}"
 	do
 	  DLRESULT="${DLRESULT/$rep/''}"
 	done
-	DLRESULT="$(echo "$DLRESULT" | grep 'DASH video')"
+	DLRESULT="$(echo "$DLRESULT" | grep 'DASH')"
 
 	while [[ $? -ne 0 ]]
 	do
 		sleep 0.500
-		DLRESULT="$(youtube-dl -F $URL | grep 'DASH video')"
+		DLRESULT="$(youtube-dl -F $URL | grep 'DASH')"
 		for rep in "${REPS[@]}"
 		do
 		  DLRESULT="${DLRESULT/$rep/''}"
 		done
-		DLRESULT="$(echo "$DLRESULT" | grep 'DASH video')"
+		DLRESULT="$(echo "$DLRESULT" | grep 'DASH')"
 	done
 
 	END=$(date +%s.%N)
@@ -59,14 +63,11 @@ do
 
 
 	while read -r line ; do
-		echo 'Representation '$numOfReps' ready. It took '$DIFF' Seconds.'
-		echo "$line"
-		REPS[$numOfReps]=${line%'video only'*}
+		echo "$line"' '$DIFF
+		REPS[$numOfReps]=${line%'DASH'*}
 		((numOfReps++))	
 	done < <(echo "${DLRESULT}")
 done
 
-echo 'Deleting Video ..'
 python youtubeAPI/delete_video.py --video-id $ID
-
-echo 'Done.'
+rm $TMPVIDEO
