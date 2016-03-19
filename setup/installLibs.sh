d69656b245a33dd0e6d5c3ba57ea1c470920775d
Armin Trattnig <armin.trattnig@gmx.at>
2016-03-19 17:29:21 +0100

    Updated installLibs

--- setup/installLibs.sh
+++ setup/installLibs.sh
@@ -11,7 +11,7 @@ cd libav
 git checkout tags/v11.4
 ./configure  --enable-gpl --enable-nonfree --enable-version3 --enable-shared --enable-libx264 --enable-libfaac --enable-libmp3lame --enable-libvpx --enable-libxvid --enable-libvorbis
 make
-make install
+sudo make install
 cd ..
 
 cd qpsnr
@@ -20,5 +20,21 @@ chmod +x qpsnr
 sudo cp qpsnr /usr/local/bin
 cd ..
 
+sudo apt-get install python-pip
+sudo pip install --upgrade google-api-python-client
 
+sudo apt-get install subversion
+svn co https://svn.code.sf.net/p/gpac/code/trunk/gpac gpac
+cd gpac
+sudo apt-get install zlib-bin zlib1g zlib1g-dev zlibc
+./configure --use-js=no --disable-3d --disable-oss-audio --disable-x11-xv --disable-ssl --use-jpeg=no --use-png=no
+make
+sudo make install
+cd ..
 
+sudo curl https://yt-dl.org/downloads/2016.03.06/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+sudo apt-get -y install wondershaper
+
+sudo ldconfig
