772805303f762cfe27a771212a78d9bfb00c6ef1
Armin Trattnig <armin.trattnig@gmx.at>
2016-09-29 13:03:17 +0200

    upd scriptf

--- scripts/measureBitcodinPSNR.sh
+++ scripts/measureBitcodinPSNR.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+OUTPUTDIR=$1
+ORIGINALFILE=$2
 
-processDash () {
-	cd $1
-	for repDir in *video*mp4 ; do
-		echo "/media/sf_shared/Bitcodin/$1$repDir"
-	done
-	cd ..
-} 
+echo 'Output Dir='$OUTPUTDIR
+echo 'Original File='$ORIGINALFILE
 
-for videoDir in */ ; do
-    	processDash $videoDir
+files=""
+
+for repDir in $OUTPUTDIR*video*mp4 ; do
+		echo "$repDir"
+		files="$files $repDir"
 done
 
+psnrResults="$(qpsnr -a avg_psnr --aopts fpa=100000 -r $ORIGINALFILE $files |& tail -1)"
+arrPSNR=(${psnrResults//,/ })
+
+ssimResults="$(qpsnr -a avg_ssim --aopts fpa=100000 -r $ORIGINALFILE $files |& tail -1)"
+arrSSIM=(${ssimResults//,/ })
+
+i=1
+for repDir in $OUTPUTDIR*video*mp4 ; do
+		echo $repDir'	'${arrPSNR[$i]}'	'${arrSSIM[$i]}
+		((i++))
+done
+
+
+echo "done."
